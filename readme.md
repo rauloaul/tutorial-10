46cbc0fb2d22df33d7cf0c633e1ed6c2a20bf70c
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-05-07 22:12:00 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- readme.md
+++ readme.md
@@ -8,3 +8,13 @@
 - `Rafif's Computer: howdy!` is printed from the asynchronously spawned task.
 - The program then waits for two seconds due to `TimerFuture::new(Duration::new(2, 0)).await;` inside the asynchronously spawned task.
 - After the two-second wait, `Rafif's Computer: done!` is printed. This completes the execution of the asynchronously spawned task.
+
+## 1.3: Multiple spawn and removing drop.
+
+### Before removing drop
+![alt text](image.png)
+
+### After removing drop
+![alt text](image.png)
+
+When spawning those tasks, the sequence of results may differ from the order in which they were specified in the code due to their asynchronous execution. Before we removed the drop statement, the program exited successfully. However, after removing it, the program continued running even after executing all tasks because the drop statement was still present, leading the program to expect additional tasks to be spawned.
--- src/main.rs
+++ src/main.rs
@@ -105,11 +105,21 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Rafif's Computer: done!");
     });
-
+    spawner.spawn(async {
+        println!("Rafif's Computer: howdy!2");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Rafif's Computer: done!2");
+    });
+    spawner.spawn(async {
+        println!("Rafif's Computer: howdy!3");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Rafif's Computer: done!3");
+    });
     println!("Rafif's Computer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
